7b98bd5c45ad46ea4f08ca2a7a839e30ae1dcb83
Krishan Roy <krishanroy@pursuit.org>
2019-10-20 22:26:44 -0400

    explore kotlin lateinit property into more detail understand the necessity and the rules to use lateinit properties and how to check if the lateinit var is actually initialized

--- src/lateinit/LateInit.kt
+++ src/lateinit/LateInit.kt
@@ -1,7 +1,27 @@
 package lateinit
 
-lateinit var myData: MyData //it cannot be val as it has to be initialized later so it cannot have final properties under the hood
-
+/**
+*   If we do not initialize a lateinit property, a specific error is
+    thrown -->UninitializedPropertyAccessException is thrown
+*   they cannot be nullable, so a lateinit property cannot be null.
+    the reason we have lateinit is that we do not want to deal with null properties.
+*   it cannot be val as it has to be initialized later so it cannot have final properties under the hood
+ *
+ *  Cannot be primitive type
+ */
+lateinit var myData: MyData
 class MyData {
 
+}
+class MyClass{
+    lateinit var lateInitVar: String
+    fun initializationLogic(){
+        println(this::lateInitVar.isInitialized) //prints false
+        lateInitVar = "some value to initialize"
+        println(this::lateInitVar.isInitialized) //prints true
+    }
+}
+fun main(){
+    MyClass().initializationLogic() //false
+                                    //true
 }
