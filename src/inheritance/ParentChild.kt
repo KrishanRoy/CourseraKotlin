43b8045c71d35f79ca271adf1112a4aa4bf6e068
Krishan Roy <krishanroy@pursuit.org>
2019-10-26 12:07:48 -0400

    explore parent child initialization complexity in more detail

--- src/inheritance/ParentChild.kt
+++ src/inheritance/ParentChild.kt
@@ -8,9 +8,19 @@ class Child(name: String) : Parent(name) {
     override fun something() = println("I am a child")
 }
 
+open class Parent2 {
+    init { print("parent ") }
+}
+class Child2 : Parent2() {
+    init { print("child ") }
+}
+
 fun main() {
     val parent1 = Parent("Parent")
     parent1.something() //should print "I am Parent" due to compile time Polymorphism
     val parent = Child("child")
     parent.something()//should print "I am child" which is called runtime polymorphism
+    //-----
+    Child2() //it should print in parent child sequence. TO create an instance of the subclass,
+    // the compiler initialize all the inherited property of the superclass first and then it initializes Child constructor.
 }
